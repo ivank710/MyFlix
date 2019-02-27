c75723683d04083129885eb0b5dbe6b6efa5ab23
Ivan Kwong <ivan.kwong710@gmail.com>
2019-02-26 18:09:40 -0800

    Add app component and root component, and react router

---
+++ myFlix/frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>MyFlix </h1>
+  </div>
+);
+
+export default App;
--- myFlix/frontend/myFlix.jsx
+++ myFlix/frontend/myFlix.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
-import {signup, login, logout} from './util/session_api_util'
+import {signup, login, logout} from './actions/session_actions'
 
 window.signup = signup;
 window.login = login;
 window.logout = logout;
 
 document.addEventListener("DOMContentLoaded", () => {
+  //const store = configureStore();
+  
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  window.getState = store.getState;  
+  window.dispatch = store.dispatch;
+
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to MyFlix</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
