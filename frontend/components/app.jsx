523da45265492b2b3753c902a1c7b709b70d644d
Ivan Kwong <ivan.kwong710@gmail.com>
2019-02-27 17:00:14 -0800

    Move nested folder to root level

---
+++ frontend/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import sign_in_form_container from '../components/session_form/sign_in_form_container';
+import signup_form_container from '../components/session_form/signup_form_container';
+import {AuthRoute} from '../util/route_util';
+
+const App = () => (
+  <div>
+    <header>
+      
+      <h1 className="head_logo"> MyFlix </h1>
+    </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={sign_in_form_container} />
+      <AuthRoute exact path="/signup" component={signup_form_container} />
+    </Switch>
+  </div>
+);
+
+export default App;
