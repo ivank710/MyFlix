3f1fda86c56d9d7278982c4b15fc8c0df03b1683
Ivan Kwong <ivan.kwong710@gmail.com>
2019-02-27 17:03:03 -0800

    modify app.jsx

--- myFlix/frontend/components/app.jsx
+++ myFlix/frontend/components/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import sign_in_form_container from '../components/session_form/sign_in_form_container';
 import signup_form_container from '../components/session_form/signup_form_container';
-import {AuthRoute} from '../util/route_util';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
     <header>
-      
+
       <h1 className="head_logo"> MyFlix </h1>
     </header>
     <Switch>
