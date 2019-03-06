93e5970a1e1c2d53ab1737019daa43ee6aac04bf
Ivan Kwong <ivan.kwong710@gmail.com>
2019-03-06 11:05:25 -0800

    Add lists reducer

---
+++ frontend/reducers/lists_reducer.js
@@ -0,0 +1,16 @@
+import {RECEIVE_MOVIE} from '../actions/movies_actions';
+import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
+
+const listsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+    case RECEIVE_MOVIE:
+      return Object.assign({}, state, { [action.movie.id]: action.movie });
+    default:
+      return state;
+  }
+};
+
+export default listsReducer;
